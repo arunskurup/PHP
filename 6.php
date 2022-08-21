9a5fdeafd62ab032d607ff4662a9e752ede138b8
Arun S Kurup <arunskurup81@gmail.com>
2022-08-21 20:15:07 +0530

    Practice5

--- 6.php
+++ 6.php
@@ -2,5 +2,9 @@
 
   function test($str,$num)
     {
-        return substr($str ,0 ,$num);
+        return substr($str ,0 ,$num).substr($str,$num+1,strlen($str)-$num);
     }
+
+    echo test("Python", 1)."\n";
+    echo test("Python", 0)."\n";
+    echo test("Python", 4)."\n";
