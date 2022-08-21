4d0f4bae9ffb857f422477f1f5e88d430020f570
Arun S Kurup <arunskurup81@gmail.com>
2022-08-21 20:55:48 +0530

    Practice12

--- 10.php
+++ 10.php
@@ -5,7 +5,9 @@
     return $num % 3 == 0 || $num % 7 == 0 ;
   }
 
-  var_dump(test(3));
-  var_dump(test(14));
-  var_dump(test(12));
-  var_dump(test(37));
+//   var_dump(test(3));
+//   var_dump(test(14));
+//   var_dump(test(12));
+//   var_dump(test(37));
+  $num = (int)readline("Enter the Number : ");
+  var_dump(test($num));
